6abbefd2257772db6cccbba7bed454fd95442014
MetaCapFin <advisor@metacap.finance>
2025-03-21 12:22:37 -0500

    Update send_email.js

--- api/send_email.js
+++ api/send_email.js
@@ -5,15 +5,17 @@ export default async function handler(req, res) {
         const { firstName, lastName, email, company, questions } = req.body;
 
         let transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false, // true for 465, false for other ports
             auth: {
-                user: process.env.EMAIL_USER, // Replace with your email
-                pass: process.env.EMAIL_PASS   // Replace with your email password
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
             }
         });
 
         let mailOptions = {
-            from: email,
+            from: 'support@ygrene.vip',
             to: 'carlos.alvarez@ygrene.com',
             subject: 'Form Submission from Landing Page',
             text: `First Name: ${firstName}\nLast Name: ${lastName}\nEmail: ${email}\nCompany: ${company}\nQuestions: ${questions}`
