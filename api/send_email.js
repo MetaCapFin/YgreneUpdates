9d8d52ffdd5b69f074ca9230e58e1cbb8065e296
MetaCapFin <advisor@metacap.finance>
2025-03-21 12:07:12 -0500

    Update send_email.js

--- api/send_email.js
+++ api/send_email.js
@@ -21,11 +21,12 @@ export default async function handler(req, res) {
 
         try {
             await transporter.sendMail(mailOptions);
-            res.status(200).send('Email sent successfully!');
+            res.status(200).json({ message: 'Email sent successfully!' });
         } catch (error) {
-            res.status(500).send('Failed to send email.');
+            console.error('Error sending email:', error);
+            res.status(500).json({ error: 'Failed to send email.' });
         }
     } else {
-        res.status(405).send('Method Not Allowed');
+        res.status(405).json({ error: 'Method Not Allowed' });
     }
 }
